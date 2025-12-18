39ba8f16e5180627c666edcbd3ef3c4dd98e11b5
Andrew Souza <andrewgouveasouza@gmail.com>
2025-12-17 23:15:54 -0300

    fix: moved files to preserve better organizationn

--- coreops-api/src/app/modules/auth/auth.routes.ts
+++
@@ -1,6 +0,0 @@
-import { FastifyInstance } from "fastify"
-import { loginController } from "./auth.controller"
-
-export async function authRoutes(app: FastifyInstance) {
-  app.post("/login", loginController)
-}

4b16404248dbd53363dee5cc5b9123770d53d5a7
Andrew Souza <andrewgouveasouza@gmail.com>
2025-12-17 23:13:43 -0300

    feat(auth): add schema for organization bootstrap registration

---
+++ coreops-api/src/app/modules/auth/controllers/register.controller.ts
@@ -0,0 +1,16 @@
+import { FastifyReply, FastifyRequest } from "fastify"
+import { registerSchema } from "../dtos/register.schemas"
+import { registerOrganizationAdmin } from "../services/register.service"
+
+export async function registerController(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const { organizationName, adminName, email, password } = registerSchema.parse(
+    request.body,
+  )
+
+  await registerOrganizationAdmin(organizationName, adminName, email, password)
+
+  reply.status(201).send({ message: "Orgainzation created" })
+}

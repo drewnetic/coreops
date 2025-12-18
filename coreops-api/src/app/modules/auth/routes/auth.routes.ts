cbcc0dfbd29551dbfa61453716f75597a3bc3eca
Andrew Souza <andrewgouveasouza@gmail.com>
2025-12-18 13:56:19 -0300

    feat(auth): add refresh and logout endpoints

--- coreops-api/src/app/modules/auth/routes/auth.routes.ts
+++ coreops-api/src/app/modules/auth/routes/auth.routes.ts
@@ -1,10 +1,17 @@
 import { FastifyInstance } from "fastify"
 import { loginController } from "../controllers/auth.controller"
 import { registerController } from "../controllers/register.controller"
+import {
+  logoutController,
+  refreshController,
+} from "../controllers/token.controller"
 
 export async function authRoutes(app: FastifyInstance) {
   app.post("/login", loginController)
 
   // DEV ONLY
   app.post("/register", registerController)
+
+  app.post("/refresh", refreshController)
+  app.post("/logout", logoutController)
 }

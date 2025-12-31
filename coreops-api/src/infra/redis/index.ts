04b2111a309999cdda428f8c441bf4de44b25e71
Andrew Souza <andrewgouveasouza@gmail.com>
2025-12-31 17:41:14 -0300

    chore(redis): fix docker redis connection using service hostname

--- coreops-api/src/infra/redis/index.ts
+++ coreops-api/src/infra/redis/index.ts
@@ -3,4 +3,4 @@ import Redis from "ioredis"
 export const redis =
   process.env.NODE_ENV === "test"
     ? new Redis({ host: "127.0.0.1", port: 6379, lazyConnect: true })
-    : new Redis()
+    : new Redis(process.env.REDIS_URL!)

15578725c71081f850e3016cfbcfd40b0a652432
Andrew Souza <andrewgouveasouza@gmail.com>
2025-12-31 23:35:28 -0300

    chore(deploy): configure production database connection

--- coreops-api/src/infra/redis/index.ts
+++ coreops-api/src/infra/redis/index.ts
@@ -3,4 +3,8 @@ import Redis from "ioredis"
 export const redis =
   process.env.NODE_ENV === "test"
     ? new Redis({ host: "127.0.0.1", port: 6379, lazyConnect: true })
-    : new Redis(process.env.REDIS_URL!)
+    : new Redis(process.env.REDIS_URL!, {
+        tls: {},
+        maxRetriesPerRequest: null,
+        enableReadyCheck: false,
+      })
